27db5b2ae188c6e7dd4b56ce8ed58670a1bc5a23
iaroki <iaroki@protonmail.com>
2020-12-08 18:24:42 +0200

    add YAML parser

---
+++ parser.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Topic struct {
+	Name              string `yaml:"name"`
+	Partitions        int    `yaml:"partitions"`
+	ReplicationFactor int    `yaml:"replicationFactor"`
+	RetentionMs       string `yaml:"retention"`
+	CleanupPolicy     string `yaml:"cleanupPolicy"`
+}
+
+type Topics struct {
+	Tpcs []Topic `topics`
+}
+
+func getYaml() Topics {
+	//fmt.Println("Parsing YAML file")
+	//
+	//var fileName string
+	//flag.StringVar(&fileName, "f", "", "YAML file to parse.")
+	//flag.Parse()
+	//
+	//if fileName == "" {
+	//	fmt.Println("Please provide yaml file by using -f option")
+	//	return
+	//}
+	fileName := "topics.yaml"
+	yamlFile, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Printf("Error reading YAML file: %s\n", err)
+	}
+
+	var topics Topics
+	err = yaml.Unmarshal(yamlFile, &topics)
+	if err != nil {
+		fmt.Printf("Error parsing YAML file: %s\n", err)
+	}
+
+	//for i, topic := range topics.Tpcs {
+	//	fmt.Println(i, topic.Name, topic.Partitions, topic.ReplicationFactor, topic.RetentionMs, topic.CleanupPolicy)
+	//}
+
+	return topics
+
+}
