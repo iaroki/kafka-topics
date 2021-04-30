4834e1f6a55832d873c7d86220cb26efdb217f91
iaroki <iaroki@protonmail.com>
2021-04-30 14:35:35 +0300

    remove obsolete env vars

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"os"
-)
-
-var topicVersion = os.Getenv("TOPIC_VERSION")
-var kafkaBroker = os.Getenv("KAFKA_BROKER")
-var kafkaUser = os.Getenv("KAFKA_USER")
-var kafkaPass = os.Getenv("KAFKA_PASS")
-
 func main() {
 
 	initApp()
